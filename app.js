bf8ea6a734b44e2578fb1a31bb2626d971b61e1a
tnguyen303 <tringuyen552911@gmail.com>
2018-09-09 20:07:08 -0400

    working verify code

--- app.js
+++ app.js
@@ -41,32 +41,95 @@ const employeeList = [
   }
 ];
 
+const activeList = [];
+
+const render = function () {
+  $('#content').empty();
+  if (activeList.length !== 0) {
+    for (i = 0; i < activeList.length; i++) {
+      //create a div with each element of array
+      $('#content').append(`<div>${activeList[i].name}</div>`);
+      $('#content').append(`<div>${activeList[i].officeNum}</div>`);
+      $('#content').append(`<div>${activeList[i].phoneNum}</div>`);
+      $('#content').append(`<div><br></div>`);
+    }
+  }
+  else {
+    for (i = 0; i < employeeList.length; i++) {
+      //create a div with each element of array
+      $('#content').append(`<div>${employeeList[i].name}</div>`);
+      $('#content').append(`<div>#${employeeList[i].officeNum}</div>`);
+      $('#content').append(`<div>${employeeList[i].phoneNum}</div>`);
+      $('#content').append(`<div><br></div>`);
+    }
+  }
+}
+
+const print = function (e) {
+  e.preventDefault();
+  render();
+}
+
+const verify = function (e) {
+  $('#content').empty();
+  e.preventDefault();
+  $('#content').append(`<input id="userInput" placeholder="John">`);
+  $('#content').append(`<button id="verifyButton">Verify</button>`);
+  $('#verifyButton').on('click', innerVerify);
+}
+
+const innerVerify = function () {
+  if(employeeList.some(e => e.name.toLowerCase() === $('#userInput').val().toLowerCase()) === true){
+    $('#content').append(`<div>Employee Found</div>`);
+  }
+  else{
+    $('#content').append(`<div>Employee Not Found</div>`);
+  }
+}
+
+
+
+$('#print').on('click', render);
+$('#verify').on('click', verify);
+$('#lookup').on('click', lookup);
+$('#contains').on('click', contains);
+$('#update').on('click', update);
+$('#add').on('click', add);
+$('#delete').on('click', deleteFunc);
+
+
+
+
+
+
+
+
 //check which command user wants to run
-const command = prompt('enter a command (print, verify, lookup, contains, update, add, delete):');
-if (command === 'print') {
-  print()
-}
-else if (command === 'verify') {
-  verify()
-}
-else if (command === 'lookup') {
-  lookup()
-}
-else if (command === 'contains') {
-  contains()
-}
-else if (command === 'update') {
-  update()
-}
-else if (command === 'add') {
-  add()
-}
-else if (command === 'delete') {
-  deletefunc()
-}
-else {
-  render('no such function!')
-}
+// const command = prompt('enter a command (print, verify, lookup, contains, update, add, delete):');
+// if (command === 'print') {
+//   print()
+// }
+// else if (command === 'verify') {
+//   verify()
+// }
+// else if (command === 'lookup') {
+//   lookup()
+// }
+// else if (command === 'contains') {
+//   contains()
+// }
+// else if (command === 'update') {
+//   update()
+// }
+// else if (command === 'add') {
+//   add()
+// }
+// else if (command === 'delete') {
+//   deletefunc()
+// }
+// else {
+//   render('no such function!')
+// }
 
 
 
@@ -74,105 +137,105 @@ else {
 
 
 
-//commands live here
-function print() {
-  render(`Total number of employees: ${employeeList.length}`);
-  render(`-------------------------------------------------------`);
-  for (i = 0; i < employeeList.length; i++) {
-    render(`name: ${employeeList[i].name}`);
-    render(`officeNum: ${employeeList[i].officeNum}`);
-    render(`phoneNum: ${employeeList[i].phoneNum}`);
-    render(`-------------------------------------------------------`);
-  }
-}
+// //commands live here
+// function print() {
+//   render(`Total number of employees: ${employeeList.length}`);
+//   render(`-------------------------------------------------------`);
+//   for (i = 0; i < employeeList.length; i++) {
+//     render(`name: ${employeeList[i].name}`);
+//     render(`officeNum: ${employeeList[i].officeNum}`);
+//     render(`phoneNum: ${employeeList[i].phoneNum}`);
+//     render(`-------------------------------------------------------`);
+//   }
+// }
 
-function verify() {
-  const nameQuery = prompt('Enter employee name:').toLowerCase();
-  let isVerified = false;
-  for (i = 0; i < employeeList.length; i++) {
-    if (nameQuery === employeeList[i].name.toLowerCase()) {
-      render('true');
-      isVerified = true;
-    }
-  }
-  if (isVerified === false) {
-    render('No such name, check spelling!');
-  }
-}
+// function verify() {
+//   const nameQuery = prompt('Enter employee name:').toLowerCase();
+//   let isVerified = false;
+//   for (i = 0; i < employeeList.length; i++) {
+//     if (nameQuery === employeeList[i].name.toLowerCase()) {
+//       render('true');
+//       isVerified = true;
+//     }
+//   }
+//   if (isVerified === false) {
+//     render('No such name, check spelling!');
+//   }
+// }
 
-function lookup() {
-  const nameQuery = prompt('Enter employee name:').toLowerCase();
-  let isVerified = false;
-  for (i = 0; i < employeeList.length; i++) {
-    if (nameQuery === employeeList[i].name.toLowerCase()) {
-      render(`name: ${employeeList[i].name}`);
-      render(`officeNum: ${employeeList[i].officeNum}`);
-      render(`phoneNum: ${employeeList[i].phoneNum}`);
-      isVerified = true;
-    }
-  }
-  if (isVerified === false) {
-    render('No such name, check spelling!');
-  }
-}
+// function lookup() {
+//   const nameQuery = prompt('Enter employee name:').toLowerCase();
+//   let isVerified = false;
+//   for (i = 0; i < employeeList.length; i++) {
+//     if (nameQuery === employeeList[i].name.toLowerCase()) {
+//       render(`name: ${employeeList[i].name}`);
+//       render(`officeNum: ${employeeList[i].officeNum}`);
+//       render(`phoneNum: ${employeeList[i].phoneNum}`);
+//       isVerified = true;
+//     }
+//   }
+//   if (isVerified === false) {
+//     render('No such name, check spelling!');
+//   }
+// }
 
-function contains() {
-  const nameQuery = prompt('Enter part of employee name:').toLowerCase();
-  for (i = 0; i < employeeList.length; i++) {
-    if (employeeList[i].name.toLowerCase().includes(nameQuery)) {
-      render(`name: ${employeeList[i].name}`);
-      render(`officeNum: ${employeeList[i].officeNum}`);
-      render(`phoneNum: ${employeeList[i].phoneNum}`);
-      render(`-------------------------------------------------------`);
-    }
-  }
-}
+// function contains() {
+//   const nameQuery = prompt('Enter part of employee name:').toLowerCase();
+//   for (i = 0; i < employeeList.length; i++) {
+//     if (employeeList[i].name.toLowerCase().includes(nameQuery)) {
+//       render(`name: ${employeeList[i].name}`);
+//       render(`officeNum: ${employeeList[i].officeNum}`);
+//       render(`phoneNum: ${employeeList[i].phoneNum}`);
+//       render(`-------------------------------------------------------`);
+//     }
+//   }
+// }
 
-function update() {
-  const nameQuery = prompt('Enter employee name:').toLowerCase();
-  let nameVerified = false;
-  let fieldVerified = false;
-  for (i = 0; i < employeeList.length; i++) {
-    if (nameQuery === employeeList[i].name.toLowerCase()) {
-      nameVerified = true;
-      const fieldToChange = prompt('Enter field to change:');
-      if (employeeList[1].hasOwnProperty(fieldToChange)) {
-        fieldVerified = true;
-        const newValue = prompt('Enter new value:');
-        employeeList[i][fieldToChange] = newValue;
-        render(`name: ${employeeList[i].name}`);
-        render(`officeNum: ${employeeList[i].officeNum}`);
-        render(`phoneNum: ${employeeList[i].phoneNum}`);
-      }
-    }
-  }
-  if (nameVerified === false) {
-    render('No such name, check spelling!');
-  }
-  if (fieldVerified === false) {
-    render('No such field, check spelling!');
-  }
-}
+// function update() {
+//   const nameQuery = prompt('Enter employee name:').toLowerCase();
+//   let nameVerified = false;
+//   let fieldVerified = false;
+//   for (i = 0; i < employeeList.length; i++) {
+//     if (nameQuery === employeeList[i].name.toLowerCase()) {
+//       nameVerified = true;
+//       const fieldToChange = prompt('Enter field to change:');
+//       if (employeeList[1].hasOwnProperty(fieldToChange)) {
+//         fieldVerified = true;
+//         const newValue = prompt('Enter new value:');
+//         employeeList[i][fieldToChange] = newValue;
+//         render(`name: ${employeeList[i].name}`);
+//         render(`officeNum: ${employeeList[i].officeNum}`);
+//         render(`phoneNum: ${employeeList[i].phoneNum}`);
+//       }
+//     }
+//   }
+//   if (nameVerified === false) {
+//     render('No such name, check spelling!');
+//   }
+//   if (fieldVerified === false) {
+//     render('No such field, check spelling!');
+//   }
+// }
 
-function add() {
-  const nameToAdd = prompt('Enter employee name:');
-  const officeNumToAdd = prompt('Enter office number:');
-  const phoneNumToAdd = prompt('Enter phone number:');
-  var newEmployee = { name: nameToAdd, officeNum: officeNumToAdd, phoneNum: phoneNumToAdd };
-  employeeList.push(newEmployee);
-  render(`${nameToAdd} has been successfully added to employee list!`);
-  render(`-------------------------------------------------------`);
-  print()
-}
+// function add() {
+//   const nameToAdd = prompt('Enter employee name:');
+//   const officeNumToAdd = prompt('Enter office number:');
+//   const phoneNumToAdd = prompt('Enter phone number:');
+//   var newEmployee = { name: nameToAdd, officeNum: officeNumToAdd, phoneNum: phoneNumToAdd };
+//   employeeList.push(newEmployee);
+//   render(`${nameToAdd} has been successfully added to employee list!`);
+//   render(`-------------------------------------------------------`);
+//   print()
+// }
 
-function deletefunc() {
-  const nameQuery = prompt('Enter employee name:').toLowerCase();
-  for (i = 0; i < employeeList.length; i++) {
-    if (nameQuery === employeeList[i].name.toLowerCase()) {
-      employeeList.splice(i, 1);
-    }
-  }
-  render(`${nameQuery} has been successfully removed from employee list!`);
-  render(`-------------------------------------------------------`);
-  print()
-}
+// function deletefunc() {
+//   const nameQuery = prompt('Enter employee name:').toLowerCase();
+//   for (i = 0; i < employeeList.length; i++) {
+//     if (nameQuery === employeeList[i].name.toLowerCase()) {
+//       employeeList.splice(i, 1);
+//     }
+//   }
+//   render(`${nameQuery} has been successfully removed from employee list!`);
+//   render(`-------------------------------------------------------`);
+//   print()
+// }
